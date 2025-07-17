62a86e4512108c3db0cca6494cc072b60730a5f3
Tippztraders <tippzcashtraders@gmail.com>
2025-07-17 19:53:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const products = [
     name: "32L Samsung Microwave",
     price: "N$950",
     condition: "Pre-Loved",
-    // status: "SOLD"
+    status: "SOLD"          // status: "SOLD" 
   },
   {
     images: ["PH3a.jpg", "PH3b.jpg"],
